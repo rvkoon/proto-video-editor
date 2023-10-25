9cccd15f769d6de897955dd4ef16001a3208127a
rvkoon <koontchak@gmail.com>
2023-10-26 00:32:24 +0200

    no video found

--- src/app/video/[videoId]/edit/page.tsx
+++ src/app/video/[videoId]/edit/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 import { useVideoEditorContext } from "@/features/video-editor/context";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function EditVideo() {
   const { videoState, isLoadingVideo } = useVideoEditorContext();
@@ -8,5 +10,16 @@ export default function EditVideo() {
     return <span className="loading loading-ring loading-lg"></span>;
   }
 
-  return <div>{videoState!.settings.id}</div>;
+  if (!videoState) {
+    return (
+      <div className="flex flex-col gap-4 justify-center items-center mt-24">
+        <div>Video not found</div>
+        <Link href="/" className="btn btn-primary">
+          Back Home
+        </Link>
+      </div>
+    );
+  }
+
+  return <div>{videoState?.settings.id}</div>;
 }
